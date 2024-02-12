3403405f5db2509b305bf070b6e41db6eec1aaeb
Gallon Zhou <gallonzhou04@gmail.com>
2024-02-12 22:39:49 +1300

    docs: Added Documentation To The Main Module

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+"""The main driver code."""
 import os
 import pygame
 
 from src.app.app import App
 
+from src.constants.constants import CAPTION
+
 pygame.init()
 
 
 def main():
+    """Gets called if this is the module containing the main driver code."""
+
     os.environ["SDL_VIDEO_CENTERED"] = "1"
+
+    pygame.display.set_caption(CAPTION)
 
     app = App()
     app.run()
